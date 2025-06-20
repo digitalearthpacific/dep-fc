dcd80d04b1ebda67fa5a7acc1c3c5a206e9a5e85
Jesse Anderson <jesse.anderson@gmail.com>
2025-06-20 15:57:11 -0700

    cd, then checkout the branch

--- setup_fc.sh
+++ setup_fc.sh
@@ -1,8 +1,8 @@
 . .venv/bin/activate
 export PATH="/app/.venv/bin:$PATH"
 git clone --depth=1 https://github.com/jessjaco/fc.git
-git checkout leak-fix
 cd fc
+git checkout leak-fix
 git pull
 python setup.py build
 python setup.py install
