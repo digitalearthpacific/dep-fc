5c385af17e70f21125c4468643b2d8e786e42f3b
Jesse Anderson <jesse.anderson@gmail.com>
2025-06-20 16:37:48 -0700

    try it a different way

--- setup_fc.sh
+++ setup_fc.sh
@@ -1,8 +1,7 @@
 . .venv/bin/activate
 export PATH="/app/.venv/bin:$PATH"
-git clone --depth=1 https://github.com/jessjaco/fc.git
+git clone -b leak-fix --depth=1 https://github.com/jessjaco/fc.git
 cd fc
-git checkout leak-fix
 git pull
 python setup.py build
 python setup.py install
