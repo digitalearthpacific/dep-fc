a2daa06afeedd4203f21eb0d853aed0f075a90b3
Jesse Anderson <jesse.anderson@gmail.com>
2025-09-18 14:14:11 -0700

    fix: changed default collection root, fixes #2

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ BUCKET = os.environ.get("FC_BUCKET", "dep-public-staging")
 DATASET_ID = "fc"
 SENSOR = "ls"
 OUTPUT_COLLECTION_ROOT = os.environ.get(
-    "OUTPUT_COLLECTION_ROOT", "https://stac.staging.digitalearthpacific.org"
+    "OUTPUT_COLLECTION_ROOT", "https://stac.staging.digitalearthpacific.io"
 )
 VERSION = "test"
 NODATA = 255
