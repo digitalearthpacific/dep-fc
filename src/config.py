5eda869506af399bc095e5dadeb54efa4165d32a
Jesse Anderson <jesse.anderson@gmail.com>
2025-09-26 08:56:22 -0700

    fix: edit collections url

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ BUCKET = os.environ.get("FC_BUCKET", "dep-public-data")
 DATASET_ID = "fc"
 SENSOR = "ls"
 OUTPUT_COLLECTION_ROOT = os.environ.get(
-    "OUTPUT_COLLECTION_ROOT", "https://stac.digitalearthpacific.org"
+    "OUTPUT_COLLECTION_ROOT", "https://stac.digitalearthpacific.org/collections"
 )
 VERSION = "0.1.0"
 NODATA = 255
